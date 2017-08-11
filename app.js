68d83ace8862342f24444b3b28fddf8645507826
Victor Bitelman Verzellesi <v.verzellesi@gmail.com>
2017-08-10 21:39:16 -0300

    Creating apartments routes, creation form and details view.

--- app.js
+++ app.js
@@ -21,8 +21,15 @@ var towerSchema = new mongoose.Schema({
     apartmentsPerFloor: Number
 });
 
+var apartmentSchema = new mongoose.Schema({
+    number: Number,
+    floor: Number,
+    dwellers: String
+});
+
 var Condo = mongoose.model('Condo', condoSchema);
 var Tower = mongoose.model('Tower', towerSchema);
+var Apartment = mongoose.model('Apartment', apartmentSchema);
 
 app.get('/', function(req, res) {
     res.render('landing');
@@ -103,6 +110,60 @@ app.post('/towers', function(req, res) {
         }
     })
 });
+
+app.get('/towers/:id', function(req, res) {
+    Tower.findById(req.params.id, function(err, foundTower) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('show-tower', { tower: foundTower })
+        }
+    });
+});
+
+app.get('/apartments', function(req, res) {
+
+    Apartment.find({}, function(err, apartments) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('apartments', { apartments: apartments });
+        }
+    });
+});
+
+app.post('/apartments', function(req, res) {
+    var number = req.body.number;
+    var floor = req.body.floor;
+    var dwellers = req.body.dwellers;
+    var newApartment = {
+        number: number,
+        floor: floor,
+        dwellers: dwellers
+    };
+
+    Apartment.create(newApartment, function(err, createdApartment) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/apartments');
+        }
+    })
+});
+
+app.get('/apartments/new', function(req, res) {
+    res.render('new-apartment');
+});
+
+app.get('/apartments/:id', function(req, res) {
+    Apartment.findById(req.params.id, function(err, foundApartment) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('show-apartment', { apartment: foundApartment });
+        }
+    });
+})
 
 app.get('*', function(req, res) {
     res.send('This page does not exist!');
