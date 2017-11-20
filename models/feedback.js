a7146d5fda3df8cc260513ea3673f11786236564
Victor Bitelman Verzellesi <v.verzellesi@gmail.com>
2017-11-20 02:50:41 -0200

    Using Moment.js to show the correct "time ago" from feedbacks.

--- models/feedback.js
+++ models/feedback.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var feedbackSchema = mongoose.Schema({
     text: String,
     category: String,
+    createdAt: { type: Date, default: Date.now },
     author: {
         id: {
             type: mongoose.Schema.Types.ObjectId,
